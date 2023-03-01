6f027f095735348df4fe3d2acaeae07cbcc5cf90
Caio Alves <caioalv7@gmail.com>
2023-03-01 16:01:10 -0300

    feat: add card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { HomeModule } from './pages/home/home.module';
+
 
 @NgModule({
   declarations: [
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { HomeComponent } from './home.component';
+import { CardComponent } from 'src/app/components/card/card.component';
 
 
 @NgModule({
   declarations: [
+    HomeComponent,
     HeaderComponent,
-    HomeComponent
+    CardComponent
   ],
   imports: [
     CommonModule
